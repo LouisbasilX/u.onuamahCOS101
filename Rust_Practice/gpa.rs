7a5694167fec708686447015d540cbcf2ed31c57
Your Name <you@example.com>
2025-10-12 16:55:07 -0700

    added validator function for input

--- Rust_Practice/gpa.rs
+++ Rust_Practice/gpa.rs
@@ -49,15 +49,21 @@ fn main() {
     // Remove trailing newline
     name = name.trim().to_string();
     
-    let mut data = String::from("");
     println!("Hello, {}! this will help u calculate your GPA but first I need your grade points and grades in the order: Gradepoint Grade, ie 2 A,3 B or 2 70,3 60 so below enter your course points and grades in the valid format to get your GPA score :", name);
-    
-     io::stdin()
+    let mut data = String::from("");
+  loop{ 
+      data.clear();
+       io::stdin()
         .read_line(&mut data)
         .expect("Failed to read line");
      
      data = data.trim().to_string();
-     
+     if validate_data(&data) {
+      break;
+    }
+    else {println!("{} please enter your grade point and grades in the correct format:\n Gradepoint1 grade1,Gradepoint2 grade2,\nfor ex: 2 A,3 B,4 C or 2 78,3 76,5 90", name);}
+    }
+  
      
    loop {
      let mut res = String::from("");
@@ -73,16 +79,29 @@ fn main() {
         else if res.trim().eq_ignore_ascii_case("n"){
              let mut more_data = String::from("");
              println!("add more courses in the same format");
-             io::stdin()
+        
+          loop{   
+                  more_data.clear();
+                  io::stdin()
         .read_line(&mut more_data)
         .expect("Failed to read line");
-             data.push_str(",");
-             data.push_str(more_data.trim());
-          }
-        else {
-        println!("invalid input");
-        }
+                  more_data = more_data.trim().to_string();
+              if validate_data(&more_data) {
+                 break;
+              }
+             else {println!("{} please enter your grade point and grades in the correct format:\n Gradepoint1 grade1,Gradepoint2 grade2,\nfor ex: 2 A,3 B,4 C or 2 78,3 76,5 90", name);
+                }
+           
+              }
+            data.push_str(",");
+            data.push_str(more_data.trim());
+         }
+         else {
+          println!("Invalid Input. Enter y to proceed or n to add more courses")
+         }
+     
      }
+
      let pg = get_gra(&data);
      println!("Your GPA is {:.2}", calc_gpa(pg[0].clone(), pg[1].clone()))
      
@@ -103,3 +122,27 @@ fn main() {
     res
   }
 
+  fn is_number_string(s: &String)-> bool{
+     s.parse::<i32>().is_ok()
+  }
+
+ fn validate_data(data: &String)-> bool{
+    let mut scores = data.split(",");
+    while let Some(item) = scores.next(){
+      let item = item.to_string();
+      let scale: HashMap<&str, i32> = 
+        [("A", 5), ("B", 4), ("C", 3), ("D", 2), ("F", 0)]
+        .iter()
+        .cloned()
+        .collect();
+      let pg: Vec<String> = item.split(" ").map(|s| s.to_string()).collect();
+      let is_invalid = pg.len() < 2 || !is_number_string(&pg[0]) || (!is_number_string(&pg[1]) && !scale.contains_key(&pg[1].as_str()));
+     
+       
+      if is_invalid {
+        return false;
+      }
+     
+    }
+     true
+  }
